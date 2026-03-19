e0540732bde281d477add93a748b2b3baf77fe4a
09parthchauhan <09parthchauhan@gmail.com>
2026-03-19 11:07:40 +0530

    more about apis and html reponse

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
+posts : list[dict] = [
+    {
+        "id" : 1,
+        "name" : "parth chauhan",
+        "title" : "software engineer",
+        "age" : 23,
+    },
+    {
+        "id" : 2,
+        "name" : "khushal chauhan",
+        "title" : "doctor",
+        "age" : 24,
+    },
+]
+
+@app.get("/api/posts")
+def get_posts():
+    return posts
+
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
 def home():
-    return {"mesaage" : "hello world"}
+    return f"<h1>{posts[0]['name']}</h1>"
 
 @app.get("/name")
 def profile():
